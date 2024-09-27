48fc8999cd60d1a19709daba1bc3d67466ce7cb9
Ibrahim Khan <73376843+Mribrahimkhan360@users.noreply.github.com>
2024-09-27 11:41:24 +0600

    Update oop.php

--- 38_programming_with_vishal_OOPs/oop.php
+++ 38_programming_with_vishal_OOPs/oop.php
@@ -1,17 +1,34 @@
 <?php
-class class1{
-	function __construct(){
-		echo "construct";
-	}
-	function test()
-	{
-		// code...
-		echo "Class1_test";
-	}
-	function __destruct(){
-		echo "destruct";
-	}
+
+// Parent class
+class class1 {
+    // Constructor
+    function __construct() {
+        echo "construct<br>";
+    }
+
+    // Method in parent class
+    function test1() {
+        echo "Class1_test<br>";
+    }
+
+    // Destructor
+    function __destruct() {
+        echo "destruct<br>";
+    }
 }
-$obj= new class1();
-$obj->test();
+
+// Child class extending the parent class
+class class2 extends class1 {
+    // Method in child class
+    function test2() {
+        echo "class2 test 2<br>";
+        parent::__construct();
+    }
+}
+
+// Creating an object of the child class
+$obj = new class2(); // This will call the constructor of class1
+$obj->test2(); // Calling the method from class2
+
 ?>
